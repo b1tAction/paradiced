52b86ba25858bfc87966a217eb311a9e9ec95bd4
Kie-Chi <137579437@qq.com>
2026-04-14 19:38:57 +0800

    chore(env): update devops docker/docker-compose.
    - add system prompt to write plan to plan-xxx.md.
    - mount .claude to docker.

--- devops/run-claude.sh
+++ devops/run-claude.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-SYSTEM_PROMPT="禁止使用git config修改Git配置;编写代码时使用英文注释,文档一律使用中文编写. 代码中除去本项目代码中一些特殊的中文场景(例如BUFF、事件名称等)其余都使用英文;git commit时请不要使用Co-Authored-By;"
+SYSTEM_PROMPT="每次编写代码前以plan mode进行完整的plan，plan以markdown文件存放在/app/.claude/plan-xxx.md;用户可以更新plan计划，故正式编码前先查看plan是否更新;禁止使用git config修改Git配置;编写代码时使用英文注释,文档一律使用中文编写. 代码中除去本项目代码中一些特殊的中文场景(例如BUFF、事件名称等)其余都使用英文;git commit时请不要使用Co-Authored-By;"
 
 exec claude \
     --dangerously-skip-permissions \
